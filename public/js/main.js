900ccec6f00dbac27459f64b33fa58316c692d30
gmco <gmco.magnet@gmail.com>
2025-04-28 22:49:24 -0500

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,7 @@
+$("document").ready(function() {
+    console.log("JS loaded");
+});
+
 $("#play-btn").click(function() {
     console.log("categories");
     window.location.href = "/pages/categories.php";
