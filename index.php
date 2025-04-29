49179ac548cc3202bac28b139ed31562bd0e463e
gmco <gmco.magnet@gmail.com>
2025-04-28 22:31:48 -0500

    css

--- index.php
+++ index.php
@@ -2,4 +2,8 @@
     require_once __DIR__ . '/templates/header.php';
 ?>
 
-<h1>Welcome to boardgame</h1>
+<h1>A University in Motion</h1>
+<button>Play</button>
+<button>Rules</button>
+
+<?php require_once __DIR__ . '/templates/footer.php'; ?>
--- templates/header.php
+++ templates/header.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="/public/css/style.css">
     <link rel="icon" type="image/x-icon" href="/../public/dice.png">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="/public/js/main.js"></script>
     <!-- Add FontAwesome for the settings icon -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
