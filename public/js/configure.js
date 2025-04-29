4c4f5fbe7feb0b59311f8bb4f403e83538cf9003
gmco <gmco.magnet@gmail.com>
2025-04-29 16:27:07 -0500

    Update configure.js

--- public/js/configure.js
+++ public/js/configure.js
@@ -7,7 +7,6 @@ document.addEventListener('DOMContentLoaded', function() {
         $.ajax({
             url: '/../phpfunctions/get_a_questions.php',
             type: 'get',
-            data: {},
             success: function(response) {
                 $("#a-questions-table").append("<tr id='questions-table-labels'><td>Question</td><td>Answer</td><td></td></tr>")
                 $("#a-questions-table").append(response);
