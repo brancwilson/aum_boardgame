46a158489e329ee24b54865a6008e54d9e5ecb4f
gmco <gmco.magnet@gmail.com>
2025-05-01 23:18:25 -0500

    Update configure_game.php

--- pages/configure_game.php
+++ pages/configure_game.php
@@ -10,6 +10,27 @@
 
         </table>
     </div>
+    <h3>Add Question</h3>
+    <form id="add-question-form">
+        <table>
+            <tr>
+                <td>
+                    <p>Question: </p>
+                </td>
+                <td>
+                    <input type="text" id="add-question-question">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p>Answer: </p>
+                </td>
+                <td>
+                    <input type="text" id="add-question-answer">
+                </td>
+            </tr>
+        </table>
+    </form>
 </div>
 
 <?php require_once __DIR__ . '/../templates/footer.php'; ?>
