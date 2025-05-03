ef498778a09bd14b6fbf37ba1bfda4cefc89cd2e
gmco <gmco.magnet@gmail.com>
2025-05-03 18:45:08 -0500

    Update configure_game.php

--- pages/configure_game.php
+++ pages/configure_game.php
@@ -4,6 +4,27 @@
 <h1>Configure Questions</h1>
 
 <div>
+    <div id="add-question-container">
+        <form id="add-question-form">
+            <h3>Add Question</h3>
+            <p>Question: </p>
+            <input type="text" id="add-question-question">
+        
+            <p>Answer: </p>
+            <input type="text" id="add-question-answer">
+
+            <p>Category: </p>
+            <select name="category" id="add-question-category">
+                <option value="a">A</option>
+                <option value="b">B</option>
+                <option value="cd">CD</option>
+                <option value="e">E</option>
+                <option value="f">F</option>
+                <option value="g">G</option>
+            </select>
+            <button id="question-create-btn">Add</button>
+        </form>
+    </div>
     <div class="questions-container">
         <h2>A Questions</h2>
         <table class="questions-table" id="a-questions-table">
@@ -30,28 +51,7 @@
 
         </table>
     </div>
-    <hr>
-    <div id="add-question-container">
-        <form id="add-question-form">
-            <h3>Add Question</h3>
-            <p>Question: </p>
-            <input type="text" id="add-question-question">
-        
-            <p>Answer: </p>
-            <input type="text" id="add-question-answer">
-
-            <p>Category: </p>
-            <select name="category" id="add-question-category">
-                <option value="a">A</option>
-                <option value="b">B</option>
-                <option value="cd">CD</option>
-                <option value="e">E</option>
-                <option value="f">F</option>
-                <option value="g">G</option>
-            </select>
-            <button id="question-create-btn">Add</button>
-        </form>
-    </div>
+    
 </div>
 
 <?php require_once __DIR__ . '/../templates/footer.php'; ?>
