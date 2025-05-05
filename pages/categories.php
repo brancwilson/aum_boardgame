24126bd852660d2cfde95b5d9f82672165d05cb8
gmco <gmco.magnet@gmail.com>
2025-05-04 22:59:09 -0500

    css

--- pages/categories.php
+++ pages/categories.php
@@ -2,19 +2,21 @@
     require_once __DIR__ . '/../templates/header.php';
 ?>
 
-<h1>Categories</h1>
+<div id="categories-menu">
+    <h1>Categories</h1>
 
-<div class="category-row">
-    <button id="category-btn">A</button>
-    <button id="category-btn">B</button>
-</div>
-<div class="category-row">
-    <button id="category-btn">C</button>
-    <button id="category-btn">D</button>
-</div>
-<div class="category-row">
-    <button id="category-btn">E</button>
-    <button id="category-btn">F</button>
+    <div class="category-row">
+        <button id="category-btn">A</button>
+        <button id="category-btn">B</button>
+    </div>
+    <div class="category-row">
+        <button id="category-btn">C</button>
+        <button id="category-btn">D</button>
+    </div>
+    <div class="category-row">
+        <button id="category-btn">E</button>
+        <button id="category-btn">F</button>
+    </div>
 </div>
 
 <?php require_once __DIR__ . '/../templates/footer.php'; ?>
