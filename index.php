2b53db59abea843973f3dd01c2b9a7c8d707536e
gmco <gmco.magnet@gmail.com>
2025-05-04 22:30:42 -0500

    css

--- index.php
+++ index.php
@@ -4,9 +4,8 @@
 <link rel="stylesheet" href="/public/css/main_menu_style.css">
 <div id="game-menu">
     <h1>A University in Motion</h1>
-    <button id="play-btn">Play</button>
-    <button id="rules-btn">Rules</button>
-    <button id="settings-btn">Settings</button>
+    <button class="menu-btn" id="play-btn">Play</button>
+    <button class="menu-btn" id="settings-btn">Settings</button>
 </div>
 
 <?php require_once __DIR__ . '/templates/footer.php'; ?>
