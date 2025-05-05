49283be04ebe3cdd1938619f3aba928b3f371cb7
gmco <gmco.magnet@gmail.com>
2025-05-04 22:04:20 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     require_once __DIR__ . '/templates/header.php';
 ?>
-<link rel="stylesheet" href="/public/css/main-menu-style.css">
+<link rel="stylesheet" href="/public/css/main_menu_style.css">
 <div id="game-menu">
     <h1>A University in Motion</h1>
     <button id="play-btn">Play</button>
