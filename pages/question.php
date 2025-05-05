f35e793887192c291d4203f24f6ecaabd5768aba
gmco <gmco.magnet@gmail.com>
2025-05-04 23:57:42 -0500

    btn

--- pages/question.php
+++ pages/question.php
@@ -9,6 +9,9 @@
 <div id="question-container">
     <p>This is a test set of words to see how the text within the question bo looks and if the question is super long then maybe it will be multiple lines but who knows how the html will react</p>
 </div>
+<div id="reveal-answer-container">
+    <button id="reveal-answer-btn">Show Answer</button>
+</div>
 
 
 <?php require_once __DIR__ . '/../templates/footer.php'; ?>
