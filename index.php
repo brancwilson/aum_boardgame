2a9874da642895b049527aea09ac3114f58321d3
gmco <gmco.magnet@gmail.com>
2025-05-04 21:58:34 -0500

    index

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
     require_once __DIR__ . '/templates/header.php';
 ?>
-<h1>A University in Motion</h1>
-<button id="play-btn">Play</button>
-<button id="rules-btn">Rules</button>
-<button id="settings-btn">Settings</button>
+<div id="game-menu">
+    <h1>A University in Motion</h1>
+    <button id="play-btn">Play</button>
+    <button id="rules-btn">Rules</button>
+    <button id="settings-btn">Settings</button>
+</div>
 
 <?php require_once __DIR__ . '/templates/footer.php'; ?>
