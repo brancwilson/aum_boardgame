86a92164add19620115a50068e1a932baa547017
gmco <gmco.magnet@gmail.com>
2025-05-05 02:18:31 -0500

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     var question = document.cookie;
+    console.log("COOKIE: " + question);
     $("#done-btn").hide();
     $("#reveal-answer-btn").show();
 
     $("document").ready(function() {
         console.log("JS loaded");
         $("#question-text").text(question.split('@')[0]);
+        console.log("COOKIE: " + question);
     });
 
     $("#play-btn").on("click", function() {
@@ -27,7 +29,9 @@ document.addEventListener('DOMContentLoaded', function() {
             type: 'post',
             data: {questionCategory: questionCategory},
             success: function(random_question) {
+                document.cookie = null;
                 document.cookie = random_question;
+                console.log("COOKIE: " + question);
                 console.log(random_question);
 
                 window.location.href = "/pages/question.php";
@@ -37,11 +41,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
     $("#reveal-answer-btn").on('click', function() {
         $("#question-text").text(question.split('@')[1])
+        console.log("COOKIE: " + question);
         $(this).hide();
         $("#done-btn").show();
     });
 
     $("#done-btn").on('click', function() {
         window.location.href = "/pages/categories.php";
+        console.log("COOKIE: " + question);
     });
 });
